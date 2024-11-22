2a3a2ef24013ca4988c3bf89dc7b1fea9a1df0d9
EgorOsaulenko <egorkapomidorka4000@gmail.com>
2024-11-22 22:28:25 +0200

    1

--- app/db_actions.py
+++ app/db_actions.py
@@ -1,9 +1,9 @@
 from db import Session, Game
 
 
-def add_game(title, price, description):
+def add_game(title, price):
     with Session() as session:
-        game = Game(title=title, price=price, description=description)
+        game = Game(title=title, price=price)
         session.add(game)
         session.commit()
         session.refresh(game)
@@ -20,14 +20,13 @@ def get_game(id):
         return session.query(Game).where(Game.id == id).first()
 
 
-def update_game(id, title, price, description):
+def update_game(id, title, price):
     with Session() as session:
         game = session.query(Game).filter_by(id=id).first()
         if not game:
             return "Гра не знайдена", 404
         game.title = title
         game.price = price
-        game.description = description
         session.commit()
         return "Дані оновлено"
 
@@ -40,18 +39,3 @@ def delete_game(id):
         session.delete(game)
         session.commit()
         return "Гру видалено"
-
-
-def seed_games():
-    with Session() as session:
-        if not session.query(Game).first():
-            games = [
-                {"title": "Cyberpunk 2077", "price": 1499, "description": "Рольова гра у футуристичному світі."},
-                {"title": "The Witcher 3", "price": 999, "description": "Епічна пригодницька гра з відкритим світом."},
-                {"title": "Minecraft", "price": 799, "description": "Кубічний світ для креативності та досліджень."},
-                {"title": "Elden Ring", "price": 1399, "description": "Фентезійний екшн у відкритому світі."},
-                {"title": "FIFA 24", "price": 1599, "description": "Симулятор футболу з реалістичною графікою."}
-            ]
-            for game in games:
-                session.add(Game(**game))
-            session.commit()
